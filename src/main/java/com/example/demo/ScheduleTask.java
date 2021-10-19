50b7c7687e4386e290df132936f8b3fedf99ecce
“lichao” <1017020555@qq.com>
2021-10-19 20:37:15 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/ScheduleTask.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+//@Component
+//public class ScheduleTask {
+//
+//    @Scheduled(cron = "*/2 * * * * ?")
+//    private void task(){
+//        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+//    }
+//
+//}
