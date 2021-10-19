b43df2dc0041e7479f8f8cfb9a8a8ede857a75ee
“lichao” <1017020555@qq.com>
2021-10-19 21:13:06 +0800

    quartz

--- src/main/java/com/example/demo/config/QuartzConfig.java
+++ src/main/java/com/example/demo/config/QuartzConfig.java
@@ -1,6 +1,29 @@
 package com.example.demo.config;
 
+import com.example.demo.job.QuartzJob;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class QuartzConfig {
 
+    @Bean
+    public JobDetail printJobDetail(){
+
+        return JobBuilder.newJob(QuartzJob.class)
+                .usingJobData("message","QQQuartz")
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger printTrigger(){
+        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("0/1 * * * * ?");
+        return TriggerBuilder.newTrigger()
+                .forJob(printJobDetail())
+                .withSchedule(cronScheduleBuilder)
+                .build();
+    }
 
 }
--- src/main/java/com/example/demo/job/QuartzJob.java
+++ src/main/java/com/example/demo/job/QuartzJob.java
@@ -3,13 +3,18 @@ package com.example.demo.job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
 public class QuartzJob  extends QuartzJobBean {
-
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        
+        String msg =(String) context.getJobDetail().getJobDataMap().get("message");
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"---"+msg);
     }
 
 }
